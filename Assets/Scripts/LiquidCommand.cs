e19b751494979fe89dce6b9d360568baae2e18e8
Eren Kumru <eren_kumru@hotmail.com>
2021-10-24 15:00:33 +0300

    Initial Commit

---
+++ Assets/Scripts/LiquidCommand.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidCommand : MonoBehaviour, Command
+{
+    public void Execute()
+    {
+        //implement me!
+    }
+}
