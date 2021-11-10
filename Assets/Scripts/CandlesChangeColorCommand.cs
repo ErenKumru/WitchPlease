4a9af2141456e1d83f5ea0bcb6518d3c2b053ccc
Nur <61030013+n6parmak@users.noreply.github.com>
2021-11-11 01:06:57 +0300

    Candle commands are added [Unity] (#6)
    * candle packet is added
    
    * candle blow fire command added
    
    * change color candle is added
    
    * nessary unversion file is added
    
    * folders are rearanged
    
    * Candle commands are finished.

---
+++ Assets/Scripts/CandlesChangeColorCommand.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandlesChangeColorCommand : ICommand{
+private Candle candles;
+
+    public CandlesChangeColorCommand(Candle candles)
+    {
+        this.candles = candles;
+    }
+
+    public void Execute()
+    {
+        Debug.Log("Candles are fired");
+        candles.changeColor();
+    }
+
+    public void Undo()
+    {
+        Debug.Log("Candles are undo");
+        candles.undoChangeColor();
+    }
+
+    public void Redo()
+    {
+        Debug.Log("Candles are fired");
+        candles.changeColor();
+    }
+}
