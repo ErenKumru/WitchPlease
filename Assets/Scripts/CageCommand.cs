4a29b8b89a70aff0b46f21665eabf47ab9c87957
n6parmak <6parmaknur@gmail.com>
2021-11-13 03:02:32 +0300

    Cage command and backgroud music are added

---
+++ Assets/Scripts/CageCommand.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CageCommand: ICommand{
+private Cage cage;
+
+    public CageCommand(Cage cage)
+    {
+        this.cage = cage;
+    }
+
+    public void Execute()
+    {
+        Debug.Log("Candles are fired");
+        if (cage.transform.localPosition.x != -0.56f){
+            cage.OpenCage();
+        }
+        else{
+            cage.CloseCage();
+        }
+    }
+
+    public void Undo()
+    {
+        if (cage.transform.localPosition.x == -0.56f){
+            cage.CloseCage();
+        }
+        else{
+            cage.OpenCage();
+        }
+    }
+
+    public void Redo(){
+       // Debug.log("CANNOT REDO");
+    }
+
+}
